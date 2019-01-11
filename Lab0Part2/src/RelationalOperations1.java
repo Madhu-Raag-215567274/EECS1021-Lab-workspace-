c7819b98d9d8f7644a40f859fb1fb8e10ae31dda
Madhu-Raag-215567274 <raag@my.yorku.ca>
2019-01-10 22:46:52 -0500

    Lab0Part2

---
+++ Lab0Part2/src/RelationalOperations1.java
@@ -0,0 +1,13 @@
+
+public class RelationalOperations1 {
+
+	public static void main(String[] args) {
+		boolean p = 3<5;// True
+		boolean q = 5<3; // False
+		System.out.println("3 < 5 is " + p);
+		System.out.println("5 < 3 is " + q);
+		System.out.println("3==5 is " + (p==q));
+     
+	}
+
+}
