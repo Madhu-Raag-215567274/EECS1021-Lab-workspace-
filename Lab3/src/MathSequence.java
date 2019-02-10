7976116fe964814529d2aa4c66de8e513f66dcb5
Madhu-Raag-215567274 <raag@my.yorku.ca>
2019-02-10 03:07:57 -0500

    lab3

--- Lab3/src/MathSequence.java
+++ Lab3/src/MathSequence.java
@@ -26,16 +26,58 @@ public class MathSequence {
 			 System.out.println("Enter the number of terms in the arithmetic sequence: ");
 			 int n =input.nextInt();
 			 input.nextLine(); 
-			 int[] ia = new int[n];
+			 
+			 int[] ia = new int[n];			 
+			 for(int j=0;j<n;j++ ) {
+				 ia[j]= a1+j*r;
+			 }
+			 ////////////////////////// for console to print properly (array list)
+			 System.out.print("<");
+			 
+		     for(int k= 0 ; k<n; k++) {		
+		    if (k<0 || k<=n-2) {
+		     System.out.print(ia[k]+",");
+		    }
+		    if (k==n-1) {
+		    	 System.out.print(ia[k]);
+		    }
+		    
+			 
+		}
+			 System.out.println(">");
+			 //////////////////////////
+			 
+			 int sum=0;
+			 for(int s = 0; s<ia.length;s++ ) {
 				
-			}
+				 sum += ia[s];
+				 
+			 }
+			 System.out.println("Sum of the arithmetic sequence: "+ sum); 
+			 
+			 System.out.println("Would you like to process another sequence?(Y/N)");
+			 String yesNo= input.nextLine();
+			 
+			 if (yesNo.equals("Y")) {
+				 i=1;
+			 }	
+			 if (yesNo.equals("N")) {
+				 i=10;
+				 ///big number to escape the loop
+			 }
+				
+			}	
 			
-			
-			
+			//* Task 1 done!			 
+			 
+		 
+		 //Task 2 begins!\\
 			
 			
 			
 		}
+		
+		System.out.println("Bye!");
 		input.close();
 	}
 
