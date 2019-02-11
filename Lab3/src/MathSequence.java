f6939557178d6c43826c559da57ffdde7a369c1a
Madhu-Raag-215567274 <raag@my.yorku.ca>
2019-02-11 01:45:15 -0500

    Lab3

--- Lab3/src/MathSequence.java
+++ Lab3/src/MathSequence.java
@@ -178,7 +178,7 @@ public class MathSequence {
 				 
 				 if (c==1 ||  ia[1]-ia[0]==ia[2]-ia[1]) {
 				 System.out.print(" is an arithmetic sequence with");
-				 System.out.println(" first term "+ia[0]+","+"common difference "+ (ia[1]-ia[0])+" and the sum: "+m);
+				 System.out.println(" first term "+ia[0]+","+"common difference "+ (ia[1]-ia[0])+" and the sum "+m);
 				 System.out.println("Would you like to process another sequence? (Y/N)");
 				 yesNo= input.nextLine();
 				 
