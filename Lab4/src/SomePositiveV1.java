1275ab7f43321ce15795eeeb0ac206732b0635f7
Madhu-Raag-215567274 <raag@my.yorku.ca>
2019-02-21 20:45:18 -0500

    Lab4

---
+++ Lab4/src/SomePositiveV1.java
@@ -0,0 +1,13 @@
+
+public class SomePositiveV1 {
+
+	public static void main(String[] args) {
+		int[] ns = {-2,-3,-1,-4,5};
+		boolean allPos = false;
+		for(int i =0; i<ns.length; i++) {
+			allPos= allPos|| ns[i]>0;
+		}
+		System.out.println(" Some elements in the array are positve: "+ allPos);
+	}
+
+}
