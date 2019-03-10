774066133fbd59f9a24319d419b3251df27a69ca
Madhu-Raag-215567274 <raag@my.yorku.ca>
2019-03-09 20:58:26 -0500

    Lab5

--- Lab5/src/Account.java
+++ Lab5/src/Account.java
@@ -3,26 +3,22 @@ public class Account {
 	String name;
 	char type; 
 	double balance;
-	
+
 	Account(String name, char type , double balance){
 		this.name=name;
 		this.type =type;
 		this .balance=balance;
-//		if (type =='i') {
-//			this.balance =balance;
-//			this.name=name;
-//		}
-//		
 		
+
 	}
-	
+
 	Account(String name, char type ){
 		this.name=name;
 		this.type =type;
-		
+
 	}
 	double getBalance () {
-		return balance;
+		return this.balance;
 	}
 	String getDescription() {
 		if(type=='i') {
@@ -33,21 +29,49 @@ public class Account {
 			String desc= "Domestic account with owner "+  this.name+ " has current balance $" + this.balance;
 			return desc;
 		}
-		
-	
+
+
 	}
 	void setBalance(double newBalance) {
 		this.balance=newBalance;
 	}
 	void deposit (double deposit) {
-		if (type=='i') {
+		if (this.type=='i') {
 			this.balance = (deposit-deposit *0.10) + this.balance;
 		}
 		else {
 			this.balance = deposit + this.balance;
 		}
 	}
-	
-	
-	
+	void withdraw(double withdraw ) {
+		if (this.type=='i') {
+			this.balance = this.balance-(withdraw+withdraw *0.10) ;
+		}
+		
+		else {
+			this.balance = this.balance-(withdraw);
+		}
+		
+	}
+	void transferTo(Account acc, double amount) {
+		if (this.type=='d' && acc.type=='d') {
+			acc.balance= amount + acc.balance;
+			this.balance = this.balance-amount;
+	     }
+		else if(this.type=='i' && acc.type=='i') {
+			acc.balance= (amount-amount*0.10) + acc.balance;
+			this.balance = this.balance-(amount+amount*0.10);
+		}
+		else if(this.type=='d' && acc.type=='i') {
+			acc.balance= acc.balance+(amount-amount*0.10);
+			this.balance= this.balance-amount;
+		}
+		else {
+			this.balance= this.balance-(amount+amount*0.10);
+			acc.balance= acc.balance+amount;
+		}
+	}
+
+
+
 }
--- Lab5/src/AccountTester.java
+++ Lab5/src/AccountTester.java
@@ -46,61 +46,61 @@ public class AccountTester {
 		System.out.println("after deposit...");
 		System.out.println("acc1 current balance: " + acc1.getBalance());
 		
-//		System.out.println("=================(5)");
-//		System.out.println("acc2 current balance: " + acc2.getBalance());
-//		/* deposit into a domestic account */
-//		acc2.deposit(50.0);
-//		System.out.println("after deposit...");
-//		System.out.println("acc2 current balance: " + acc2.getBalance());
-//		
-//		System.out.println("=================(6)");
-//		System.out.println("acc3 current balance: " + acc3.getBalance());
-//		/* withdraw from an international account */
-//		acc3.withdraw(50.0);
-//		System.out.println("after withdraw...");
-//		System.out.println("acc3 current balance: " + acc3.getBalance());
-//		
-//		System.out.println("=================(7)");
-//		System.out.println("acc4 current balance: " + acc4.getBalance());
-//		/* withdraw from a domestic account */
-//		acc4.withdraw(50.0);
-//		System.out.println("after withdraw...");
-//		System.out.println("acc4 current balance: " + acc4.getBalance());
-//		
-//		System.out.println("=================(8)");
-//		System.out.println(acc2.getDescription());
-//		System.out.println(acc4.getDescription());
-//		/* transfer from a domestic account to another domestic account */
-//		acc2.transferTo(acc4, 20.0);
-//		System.out.println("after transfer...");
-//		System.out.println(acc2.getDescription());
-//		System.out.println(acc4.getDescription());
-//		
-//		System.out.println("=================(9)");
-//		System.out.println(acc1.getDescription());
-//		System.out.println(acc3.getDescription());
-//		/* transfer from an international account to another international account */
-//		acc1.transferTo(acc3, 20.0);
-//		System.out.println("after transfer...");
-//		System.out.println(acc1.getDescription());
-//		System.out.println(acc3.getDescription());
-//		
-//		System.out.println("=================(10)");
-//		System.out.println(acc2.getDescription());
-//		System.out.println(acc3.getDescription());
-//		/* transfer from a domestic account to an international account */
-//		acc2.transferTo(acc3, 30.0);
-//		System.out.println("after transfer...");
-//		System.out.println(acc2.getDescription());
-//		System.out.println(acc3.getDescription());
-//		
-//		System.out.println("=================(11)");
-//		System.out.println(acc1.getDescription());
-//		System.out.println(acc4.getDescription());
-//		/* transfer from an international account to a domestic account */
-//		acc1.transferTo(acc4, 30.0);
-//		System.out.println("after transfer...");
-//		System.out.println(acc1.getDescription());
-//		System.out.println(acc4.getDescription());
+		System.out.println("=================(5)");
+		System.out.println("acc2 current balance: " + acc2.getBalance());
+		/* deposit into a domestic account */
+		acc2.deposit(50.0);
+		System.out.println("after deposit...");
+		System.out.println("acc2 current balance: " + acc2.getBalance());
+		
+		System.out.println("=================(6)");
+		System.out.println("acc3 current balance: " + acc3.getBalance());
+		/* withdraw from an international account */
+		acc3.withdraw(50.0);
+		System.out.println("after withdraw...");
+		System.out.println("acc3 current balance: " + acc3.getBalance());
+		
+		System.out.println("=================(7)");
+		System.out.println("acc4 current balance: " + acc4.getBalance());
+		/* withdraw from a domestic account */
+		acc4.withdraw(50.0);
+		System.out.println("after withdraw...");
+		System.out.println("acc4 current balance: " + acc4.getBalance());
+		
+		System.out.println("=================(8)");
+		System.out.println(acc2.getDescription());
+		System.out.println(acc4.getDescription());
+		/* transfer from a domestic account to another domestic account */
+		acc2.transferTo(acc4, 20.0);
+		System.out.println("after transfer...");
+		System.out.println(acc2.getDescription());
+		System.out.println(acc4.getDescription());
+		
+		System.out.println("=================(9)");
+		System.out.println(acc1.getDescription());
+		System.out.println(acc3.getDescription());
+		/* transfer from an international account to another international account */
+		acc1.transferTo(acc3, 20.0);
+		System.out.println("after transfer...");
+		System.out.println(acc1.getDescription());
+		System.out.println(acc3.getDescription());
+		
+		System.out.println("=================(10)");
+		System.out.println(acc2.getDescription());
+		System.out.println(acc3.getDescription());
+		/* transfer from a domestic account to an international account */
+		acc2.transferTo(acc3, 30.0);
+		System.out.println("after transfer...");
+		System.out.println(acc2.getDescription());
+		System.out.println(acc3.getDescription());
+		
+		System.out.println("=================(11)");
+		System.out.println(acc1.getDescription());
+		System.out.println(acc4.getDescription());
+		/* transfer from an international account to a domestic account */
+		acc1.transferTo(acc4, 30.0);
+		System.out.println("after transfer...");
+		System.out.println(acc1.getDescription());
+		System.out.println(acc4.getDescription());
 	}
 }
