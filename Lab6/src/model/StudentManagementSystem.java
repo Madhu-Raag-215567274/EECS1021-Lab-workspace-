0065cf6bcb28ce900ea138457fb30cd29f182eb0
Madhu-Raag-215567274 <46466099+Madhu-Raag-215567274@users.noreply.github.com>
2019-03-18 14:40:28 -0400

    Lab6

--- Lab6/src/model/StudentManagementSystem.java
+++ Lab6/src/model/StudentManagementSystem.java
@@ -4,56 +4,64 @@ import model.Student;
 
 public class StudentManagementSystem {
 	
-	public Student [] des; 
-	int nos=0 ;
+	public Student[] des; 
+	int nos;
 	public final int MAX_NUM_STUDENTS= 100;
-	//Student[] sms;
-
 	
-	Student[] sms = new Student[MAX_NUM_STUDENTS];
+	public Student[] sms;
+	CourseRecord s;
 	
 	
 	public StudentManagementSystem(){
 		
-	Student[] sms = new Student[MAX_NUM_STUDENTS];
-//	this.sms = new Student[MAX_NUM_STUDENTS];
+	this.sms = new Student[nos];
 	this.nos = 0;
 	}
 	
 	public Student[] getStudents() {
-		this.sms = new Student [this.nos];
-			
 		return sms ;
 	}
 	
 	public void addStudent(Student a) {
-		Student[] sms = new Student[MAX_NUM_STUDENTS];
-		this.sms [this.nos] = a;
-		this.nos ++;		
+		Student [] sms1 = new Student[nos+1];
+		for(int i = 0 ; i<this.nos;i++) {
+			sms1[i]= this.sms[i];
+		}
+		sms1[nos]= a;
+		this.sms= sms1;
+		nos++;
 		
 	}
 
-
 	public String getDescription() {
-		String result ="";
+		this.des= this.sms;
+		String result ="Student management system currently stores "+ this.nos+" students:";
 		if (this.nos>0  ) {		
 			for(int i =0 ; i<this.nos; i++) {
-				//result = "Student management system currently stores "+ this.nos+" students: \n ";
-			result = des[i].getDescription();			
-				
-			}
-			
+				result +=  des[i].getDescription();				
+			}			
 		}
 		else  {
 			result = "Student management system currently stores no students.";
 			
 		}
-	
-		return result;
-
-		
+		return result;		
 
 	}
+	public int getMarks(String s, String t) {
+		
+		this.s= new CourseRecord(t);
+		
+		return this.s.getMarks();
+	}
+	
+	public void setMarks(String name, String t,int marks) {
+			
+		
+	}
+
+	
+	
 
 
 }
